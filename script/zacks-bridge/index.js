8b808b9941878e691c6228447fd13d6e1d511073
Brett550 <143304058+Brett550@users.noreply.github.com>
2026-06-08 17:41:30 -0400

    remove commented out code

--- script/zacks-bridge/index.js
+++ script/zacks-bridge/index.js
@@ -32,24 +32,6 @@ async function main() {
         console.error("Error fetching data:", error);
         process.exit(1);
     }
-    // const ticker = process.argv[2];
-
-    // if (!ticker) {
-    //     console.error("Usage: node index.js <ticker>");
-    //     process.exit(1);
-    // }
-
-    // try {
-    //     const result = await api.getData(ticker);
-
-    //     console.log(JSON.stringify(result));
-
-    //     process.exit(0);
-    // } catch (error) {
-    //     console.error("Error fetching data:", error);
-        
-    //     process.exit(1);
-    // };
 }
 
 main();
